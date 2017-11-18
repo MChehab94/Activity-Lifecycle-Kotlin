70b5d9a5be2aa0d9401071dac522eccb27ecde68
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-18 17:36:21 +0200

    Initial commit

---
+++ app/src/main/java/mchehab/com/activitylifecycledemo/MainActivity.kt
@@ -0,0 +1,35 @@
+package mchehab.com.activitylifecycledemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "state"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+}
